439c0b7e7a54a5b194a5cdd46364551ad0442e12
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-07 10:22:37 -0700

    Render smiley with React

--- smiley/src/app.js
+++ smiley/src/app.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
-import HelloWorld from './helloworld';
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 30;
 
-render(<HelloWorld />, document.getElementById('app-root'));
+const eyeOffsetX = 80;
+const eyeCenterY = 200;
+const eyeRadius = 30;
+
+const App = () => (
+
+
+     <svg width={width} height={height}>
+    <circle r={centerY - strokeWidth / 2} cx={centerX} cy={centerY}
+            fill="yellow"
+            stroke="black"
+            stroke-width={strokeWidth}
+            ></circle>
+    <circle r={eyeRadius} cx={centerX - eyeOffsetX} cy={eyeCenterY}   
+            stroke="black"
+            stroke-width="10"
+            />
+    <circle r={eyeRadius} cx={centerX + eyeOffsetX} cy={eyeCenterY}    
+            stroke="black"
+            stroke-width="10"
+            ></circle>
+            </svg>
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
